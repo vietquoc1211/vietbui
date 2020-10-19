27ba0a0dbc7ee78fd6e68894ffb9586f224f2752
Bui Quoc Viet <viet_bq@care-connect.vn>
2020-10-19 13:03:21 +0700

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/vietbui'));
+app.use(static(__dirname + '/dist/vietbui'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/vietbui/index.html'));
+  res.sendFile(join(__dirname + '/dist/vietbui/index.html'));
 });
 
 // default Heroku PORT
