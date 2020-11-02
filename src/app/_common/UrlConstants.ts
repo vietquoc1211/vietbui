90e5c9f0bbb294f12f92a1f7573172c6b56aba7c
Bui Quoc Viet <viet_bq@care-connect.vn>
2020-11-02 14:00:17 +0700

    update tinh thanh

--- src/app/_common/UrlConstants.ts
+++ src/app/_common/UrlConstants.ts
@@ -1,11 +1,11 @@
 export class UrlConstants {
 
   // local
-  public static BASE_CLIENT = 'http://localhost:1337';
-  public static BASE_API = 'http://localhost:3000/api';
+  // public static BASE_CLIENT = 'http://localhost:1337';
+  // public static BASE_API = 'http://localhost:3000/api';
 
-  // public static BASE_CLIENT = 'https://vietbui.herokuapp.com';
-  // public static BASE_API =  'https://api-vietbui.herokuapp.com/api';
+  public static BASE_CLIENT = 'https://vietbui.herokuapp.com';
+  public static BASE_API =  'https://api-vietbui.herokuapp.com/api';
   public static LOGIN = '/login';
 
   public static HOME = '/';
